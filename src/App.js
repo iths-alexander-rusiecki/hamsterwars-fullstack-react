8b895bfa725a218481b71c56fe5d46d9d72f5f98
Alexander Rusiecki <alexander.rusiecki@gmail.com>
2020-06-11 21:34:37 +0200

    tested main

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => {
             <header className="App-header">
                 <h1>My header is nice</h1>
             </header>
+            <main>
+                <h1>Main</h1>
+            </main>
         </div>
     );
 };
