ff0bd2cd7ba3caac32382f04bb7ee56223062563
Alexander Rusiecki <alexander.rusiecki@gmail.com>
2020-06-11 21:49:52 +0200

    first component and routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
 import "./App.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink,
+    Redirect,
+} from "react-router-dom";
+import HamsterWarsStartPage from "./components/HamsterWarsStartPage";
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>My header is nice</h1>
-            </header>
-            <main>
-                <h1>Main</h1>
-            </main>
-        </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1>My header is nice</h1>
+                    <nav>
+                        <NavLink exact to="/">
+                            Start
+                        </NavLink>
+                        <NavLink to="/battle">Battle</NavLink>
+                        <NavLink to="/battle/:id1/:id2">
+                            Specific match up
+                        </NavLink>
+                        <NavLink to="/matchup/:id1/:id2">Match result</NavLink>
+                        <NavLink to="/stats">Stats</NavLink>
+                        <NavLink to="/upload">upload hamster pic</NavLink>
+                    </nav>
+                </header>
+                <main className="App-main">
+                    <Switch>
+                        <Route exact path="/">
+                            <HamsterWarsStartPage />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     );
 };
 
---
+++ src/components/HamsterWarsStartPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HamsterWarsStartPage = () => {
+    return (
+        <div>
+            <h1>This is the start page</h1>
+        </div>
+    );
+};
+
+export default HamsterWarsStartPage;
